0b901cbabc760d63acd9cfd9bd737d388b2f9f69
florian-lamotte <fl.lamotte@gmail.com>
2020-02-27 22:17:42 +0100

    ajout user

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -8,16 +8,18 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class PostController extends AbstractController
 {
   /**
-  * @Route("/posts", name="blog_posts")
-  */
+   * @Route("/posts", name="blog_posts")
+   */
   public function list()
   {
-    return $this->render('post/list.html.twig');
+  	$url = $this->generateUrl('blog_post', ['id' => 1]);
+
+    return $this->render('post/list.html.twig', ['url' => $url]);
   }
 
   /**
-  * @Route("/post/{id}", name="blog_post", requirements={"id"="\d+"})
-  */
+   * @Route("/post/{id}", name="blog_post", requirements={"id"="\d+"})
+   */
   public function show(int $id)
   {
     return $this->render('post/show.html.twig', ['id' => $id]);
